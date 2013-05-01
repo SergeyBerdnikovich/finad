65d11f21e7f8d4965dace2fbfb46ac1f43924779
SergeyBerdnikovich <atom@tut.by>
2013-05-01 15:53:56 +0300

    changed advisers migration(add index)

--- db/migrate/20130426015930_create_advisers.rb
+++ db/migrate/20130426015930_create_advisers.rb
@@ -5,12 +5,17 @@ class CreateAdvisers < ActiveRecord::Migration
       t.string :address
       t.string :city
       t.string :state
-      t.string :zip
+      t.integer :zip
       t.string :phone
       t.string :url
       t.integer :stars
 
       t.timestamps
     end
+
+    add_index :advisers, :name
+    add_index :advisers, :zip
+    add_index :advisers, :stars
+    add_index :advisers, :address
   end
 end

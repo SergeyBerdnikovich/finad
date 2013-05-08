d8e052f221be511484580fc54a33887458347ca7
Dvporg <dvporg@gmail.com>
2013-05-08 07:12:49 +0000

    Lot of changes

--- app/controllers/advisers_controller.rb
+++ app/controllers/advisers_controller.rb
@@ -1,12 +1,27 @@
 class AdvisersController < ApplicationController
-  
+
+
+  def check_adviser_user
+    return true if current_adviser_user && current_adviser_user.id == @adviser.adviser_user.id
+    return false
+  end
+
+
   def show
     @adviser = Adviser.find(params[:id])
+    @owner = check_adviser_user
+    #if @adviser.photo == nil
+    #  @adviser.photo == "default.gif"
+    #end
   end
 
   def edit
-    @adviser = Adviser.find(params[:id])
-    @adviser.build_gallery unless @adviser.gallery
+    if current_adviser_user && current_adviser_user.id == @adviser.adviser_user.id
+      @adviser = Adviser.find(params[:id])
+      @adviser.build_gallery unless @adviser.gallery
+    else
+      redirect_to root_path, notice: 'Authorization failed.'
+    end
   end
 
   def update
@@ -14,12 +29,16 @@ class AdvisersController < ApplicationController
     @adviser = Adviser.find(params[:id])
 
     respond_to do |format|
-      if @adviser.update_attributes(params[:adviser])
-        format.html { redirect_to adviser_path(@adviser), notice: 'Adviser was successfully updated.' }
-        format.json { render action: 'show', status: :created, location: @adviser }
+      if current_adviser_user && current_adviser_user.id == @adviser.adviser_user.id
+        if @adviser.update_attributes(params[:adviser])
+          format.html { redirect_to adviser_path(@adviser), notice: 'Adviser was successfully updated.' }
+          format.json { render action: 'show', status: :created, location: @adviser }
+        else
+          format.html { render action: 'new' }
+          format.json { render json: @adviser.errors, status: :unprocessable_entity }
+        end
       else
-        format.html { render action: 'new' }
-        format.json { render json: @adviser.errors, status: :unprocessable_entity }
+        redirect_to root_path, notice: 'Authorization failed.'
       end
     end
   end
--- parselsx.rb
+++ parselsx.rb
@@ -18,6 +18,277 @@ config = YAML.load_file('config/database.yml')
 
 $sql = Mysql2::Client.new(sqlconf)
 
+
+
+begin
+xls = Roo::Spreadsheet.open('6.xlsx')
+firsline = true
+xls.each{|ar|
+	name = ""
+	address = ""
+	city = ""
+	state = ""
+	zip = ""
+	phone = ""
+	if firsline == false
+		name = $sql.escape(ar[1]) if ar[1] != nil
+		address = $sql.escape(ar[5]) if ar[5] != nil
+		city = $sql.escape(ar[2]) if ar[2] != nil
+		state = $sql.escape(ar[3]) if ar[3] != nil
+		zip = $sql.escape(ar[4]) if ar[4] != nil
+		zip = zip.match(/[0-9]{5}/)
+		if zip != nil
+		zip = zip[0]
+		else
+		zip = 0
+		end
+		zip = 0 if zip == ""
+		
+		phone = $sql.escape(ar[6]) if ar[6] != nil
+		url = $sql.escape(ar[7]) if ar[7] != nil
+		req = "SELECT * FROM advisers WHERE name = '#{name}' AND zip = '#{zip}'"
+		check = $sql.query(req)	
+		if check.to_a.size == 0
+			req = "INSERT INTO advisers (name,address,city,state,zip,phone,url,stars) VALUES ('#{name}','#{address}','#{city}','#{state}','#{zip}','#{phone}','#{url}',2)"
+		else
+#		p check.to_a
+		end
+		$sql.query(req)	
+
+	else
+		firsline = false
+	end
+}
+rescue Exceprion => e
+	p e
+	p e.backtrace
+end
+
+begin
+xls = Roo::Spreadsheet.open('7.xlsx')
+firsline = true
+xls.each{|ar|
+	name = ""
+	address = ""
+	city = ""
+	state = ""
+	zip = ""
+	phone = ""
+	if firsline == false
+		name = $sql.escape(ar[1]) if ar[1] != nil
+		address = $sql.escape(ar[2]) if ar[2] != nil
+		city = $sql.escape(ar[3]) if ar[3] != nil
+		state = $sql.escape(ar[4]) if ar[4] != nil
+		zip = $sql.escape(ar[5]) if ar[5] != nil
+		zip = zip.match(/[0-9]{5}/)
+		if zip != nil
+		zip = zip[0]
+		else
+		zip = 0
+		end
+		zip = 0 if zip == ""
+		
+		phone = $sql.escape(ar[6]) if ar[6] != nil
+		url = $sql.escape(ar[7]) if ar[7] != nil
+		req = "SELECT * FROM advisers WHERE name = '#{name}' AND zip = '#{zip}'"
+		check = $sql.query(req)	
+		if check.to_a.size == 0
+			req = "INSERT INTO advisers (name,address,city,state,zip,phone,url,stars) VALUES ('#{name}','#{address}','#{city}','#{state}','#{zip}','#{phone}','#{url}',2)"
+		else
+#		p check.to_a
+		end
+		$sql.query(req)	
+
+	else
+		firsline = false
+	end
+}
+rescue Exceprion => e
+	p e
+	p e.backtrace
+end
+
+begin
+xls = Roo::Spreadsheet.open('8.xlsx')
+firsline = true
+xls.each{|ar|
+	name = ""
+	address = ""
+	city = ""
+	state = ""
+	zip = ""
+	phone = ""
+	if firsline == false
+		name = $sql.escape(ar[0]) if ar[0] != nil
+		address = $sql.escape(ar[1]) if ar[1] != nil
+		city = $sql.escape(ar[2]) if ar[2] != nil
+		state = $sql.escape(ar[3]) if ar[3] != nil
+		zip = $sql.escape(ar[4]) if ar[4] != nil
+		zip = zip.match(/[0-9]{5}/)
+		if zip != nil
+		zip = zip[0]
+		else
+		zip = 0
+		end
+		zip = 0 if zip == ""
+		
+		phone = $sql.escape(ar[5]) if ar[5] != nil
+		url = ""
+		req = "SELECT * FROM advisers WHERE name = '#{name}' AND zip = '#{zip}'"
+		check = $sql.query(req)	
+		if check.to_a.size == 0
+			req = "INSERT INTO advisers (name,address,city,state,zip,phone,url,stars) VALUES ('#{name}','#{address}','#{city}','#{state}','#{zip}','#{phone}','#{url}',2)"
+		else
+#		p check.to_a
+		end
+		$sql.query(req)	
+
+	else
+		firsline = false
+	end
+}
+rescue Exceprion => e
+	p e
+	p e.backtrace
+end
+
+begin
+xls = Roo::Spreadsheet.open('9.xlsx')
+firsline = true
+xls.each{|ar|
+	name = ""
+	address = ""
+	city = ""
+	state = ""
+	zip = ""
+	phone = ""
+	if firsline == false
+		name = $sql.escape(ar[0]) if ar[0] != nil
+		address = $sql.escape(ar[1]) if ar[1] != nil
+		city = $sql.escape(ar[2]) if ar[2] != nil
+		state = $sql.escape(ar[3]) if ar[3] != nil
+		zip = $sql.escape(ar[4]) if ar[4] != nil
+		zip = zip.match(/[0-9]{5}/)
+		if zip != nil
+		zip = zip[0]
+		else
+		zip = 0
+		end
+		zip = 0 if zip == ""
+		
+		phone = $sql.escape(ar[5]) if ar[5] != nil
+		email = $sql.escape(ar[6]) if ar[6] != nil
+		url = $sql.escape(ar[7]) if ar[7] != nil
+		req = "SELECT * FROM advisers WHERE name = '#{name}' AND zip = '#{zip}'"
+		check = $sql.query(req)	
+		if check.to_a.size == 0
+			req = "INSERT INTO advisers (name,address,city,state,zip,phone,url,stars) VALUES ('#{name}','#{address}','#{city}','#{state}','#{zip}','#{phone}','#{url}',2)"
+		$sql.query(req)
+		else
+#		p check.to_a
+		end
+	else
+		firsline = false
+	end
+}
+rescue Exceprion => e
+	p e
+	p e.backtrace
+end
+
+#exit!  # ######################################################################################## ALREADY SCANNED ##############################################
+
+begin
+xls = Roo::Spreadsheet.open('5.xlsx')
+firsline = true
+xls.each{|ar|
+	name = ""
+	address = ""
+	city = ""
+	state = ""
+	zip = ""
+	phone = ""
+	if firsline == false
+		name = $sql.escape(ar[1]) if ar[1] != nil
+		address = $sql.escape(ar[5]) if ar[5] != nil
+		city = $sql.escape(ar[2]) if ar[2] != nil
+		state = $sql.escape(ar[3]) if ar[3] != nil
+		zip = $sql.escape(ar[4]) if ar[4] != nil
+		zip = zip.match(/[0-9]{5}/)
+		if zip != nil
+		zip = zip[0]
+		else
+		zip = 0
+		end
+		zip = 0 if zip == ""
+		
+		phone = $sql.escape(ar[6]) if ar[6] != nil
+		url = $sql.escape(ar[7]) if ar[7] != nil
+		req = "SELECT * FROM advisers WHERE name = '#{name}' AND zip = '#{zip}'"
+		check = $sql.query(req)	
+		if check.to_a.size == 0
+			req = "INSERT INTO advisers (name,address,city,state,zip,phone,url,stars) VALUES ('#{name}','#{address}','#{city}','#{state}','#{zip}','#{phone}','#{url}',2)"
+		else
+#		p check.to_a
+		end
+		$sql.query(req)	
+
+	else
+		firsline = false
+	end
+}
+rescue Exceprion => e
+	p e
+	p e.backtrace
+end
+
+
+begin
+xls = Roo::Spreadsheet.open('4.xlsx')
+firsline = true
+xls.each{|ar|
+	name = ""
+	address = ""
+	city = ""
+	state = ""
+	zip = ""
+	phone = ""
+	if firsline == false
+		name = $sql.escape(ar[1]) if ar[1] != nil
+		address = $sql.escape(ar[5]) if ar[5] != nil
+		city = $sql.escape(ar[2]) if ar[2] != nil
+		state = $sql.escape(ar[3]) if ar[3] != nil
+		zip = $sql.escape(ar[4]) if ar[4] != nil
+		zip = zip.match(/[0-9]{5}/)
+		if zip != nil
+		zip = zip[0]
+		else
+		zip = 0
+		end
+		zip = 0 if zip == ""
+		
+		phone = $sql.escape(ar[6]) if ar[6] != nil
+		url = $sql.escape(ar[7]) if ar[7] != nil
+		req = "SELECT * FROM advisers WHERE name = '#{name}' AND zip = '#{zip}'"
+		check = $sql.query(req)	
+		if check.to_a.size == 0
+			req = "INSERT INTO advisers (name,address,city,state,zip,phone,url,stars) VALUES ('#{name}','#{address}','#{city}','#{state}','#{zip}','#{phone}','#{url}',2)"
+		else
+#		p check.to_a
+		end
+		$sql.query(req)	
+
+	else
+		firsline = false
+	end
+}
+rescue Exceprion => e
+	p e
+	p e.backtrace
+end
+
+
+begin
 xls = Roo::Spreadsheet.open('1.xlsx')
 firsline = true
 xls.each{|ar|
@@ -33,7 +304,12 @@ xls.each{|ar|
 		city = $sql.escape(ar[3]) if ar[3] != nil
 		state = $sql.escape(ar[4]) if ar[4] != nil
 		zip = $sql.escape(ar[5]) if ar[5] != nil
-		zip.gsub!(/[^0-9]/,"")
+		zip = zip.match(/[0-9]{5}/)
+		if zip != nil
+		zip = zip[0]
+		else
+		zip = 0
+		end
 		zip = 0 if zip == ""
 		phone = $sql.escape(ar[6]) if ar[6] != nil
 		if ar[7] != nil
@@ -41,13 +317,22 @@ xls.each{|ar|
 		else
 			url = ""
 		end
-		req = "INSERT INTO advisers (name,address,city,state,zip,phone,url,stars) VALUES ('#{name}','#{address}','#{city}','#{state}',#{zip},'#{phone}','#{url}',2)"
-		$sql.query(req)	
+		req = "SELECT * FROM advisers WHERE name = '#{name}' AND zip = '#{zip}'"
+		check = $sql.query(req)	
+		if check.to_a.size == 0
+			req = "INSERT INTO advisers (name,address,city,state,zip,phone,url,stars) VALUES ('#{name}','#{address}','#{city}','#{state}','#{zip}','#{phone}','#{url}',2)"
+		$sql.query(req)
+		else
+#		p check.to_a
+		end
 	else
 		firsline = false
 	end
 }
+rescue
+end
 
+begin
 xls = Roo::Spreadsheet.open('2.xlsx')
 firsline = true
 xls.each{|ar|
@@ -63,17 +348,31 @@ xls.each{|ar|
 		city = $sql.escape(ar[2]) if ar[2] != nil
 		state = $sql.escape(ar[3]) if ar[3] != nil
 		zip = $sql.escape(ar[4]) if ar[4] != nil
-		zip.gsub!(/[^0-9]/,"")
+		zip = zip.match(/[0-9]{5}/)
+		if zip != nil
+		zip = zip[0]
+		else
+		zip = 0
+		end
 		zip = 0 if zip == ""
 		phone = $sql.escape(ar[5]) if ar[5] != nil
 		url = ""
-		req = "INSERT INTO advisers (name,address,city,state,zip,phone,url,stars) VALUES ('#{name}','#{address}','#{city}','#{state}','#{zip}','#{phone}','#{url}',2)"
-		$sql.query(req)	
+		req = "SELECT * FROM advisers WHERE name = '#{name}' AND zip = '#{zip}'"
+		check = $sql.query(req)	
+		if check.to_a.size == 0
+			req = "INSERT INTO advisers (name,address,city,state,zip,phone,url,stars) VALUES ('#{name}','#{address}','#{city}','#{state}','#{zip}','#{phone}','#{url}',2)"
+		$sql.query(req)
+		else
+#		p check.to_a
+		end
 	else
 		firsline = false
 	end
 }
+rescue
+end
 
+begin
 xls = Roo::Spreadsheet.open('3.xlsx')
 firsline = true
 xls.each{|ar|
@@ -89,13 +388,26 @@ xls.each{|ar|
 		city = $sql.escape(ar[2]) if ar[2] != nil
 		state = $sql.escape(ar[3]) if ar[3] != nil
 		zip = $sql.escape(ar[4]) if ar[4] != nil
-		zip.gsub!(/[^0-9]/,"")
+		zip = zip.match(/[0-9]{5}/)
+		if zip != nil
+		zip = zip[0]
+		else
+		zip = 0
+		end
 		zip = 0 if zip == ""
 		phone = $sql.escape(ar[5]) if ar[5] != nil
 		url = ""
-		req = "INSERT INTO advisers (name,address,city,state,zip,phone,url,stars) VALUES ('#{name}','#{address}','#{city}','#{state}','#{zip}','#{phone}','#{url}',2)"
-		$sql.query(req)	
+		req = "SELECT * FROM advisers WHERE name = '#{name}' AND zip = '#{zip}'"
+		check = $sql.query(req)	
+		if check.to_a.size == 0
+			req = "INSERT INTO advisers (name,address,city,state,zip,phone,url,stars) VALUES ('#{name}','#{address}','#{city}','#{state}','#{zip}','#{phone}','#{url}',2)"
+		$sql.query(req)
+		else
+#		p check.to_a
+		end
 	else
 		firsline = false
 	end
 }
+rescue
+end

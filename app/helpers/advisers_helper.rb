37a1b4dfefa8607a75dc6ade4ee1d35c013a8038
SergeyBerdnikovich <atom@tut.by>
2013-06-13 14:45:51 +0300

    added verified.gif to advisers/show add helpers to adviser

--- app/helpers/advisers_helper.rb
+++ app/helpers/advisers_helper.rb
@@ -1,2 +1,8 @@
 module AdvisersHelper
+  def owner?
+    current_adviser_user.try(:adviser).try(:id).try(:to_s) == params[:id]
+  end
+  def verified?
+    current_adviser_user.try(:adviser).try(:verified)
+  end
 end
